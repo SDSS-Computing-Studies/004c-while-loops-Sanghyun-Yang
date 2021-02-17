7b478629c02a4cc4bda6e71186e89edc445301e4
Sanghyun-Yang <78501215+Sanghyun-Yang@users.noreply.github.com>
2021-02-17 10:44:07 -0800

    Update problem1.py

--- problem1.py
+++ problem1.py
@@ -17,3 +17,20 @@ outputs:
 Access granted
 Access denied
 """
+username = str(input())
+password = str()
+trying1 = int()
+trying2 = int()
+while username != "admin":
+    username = str(input())
+    trying = trying1 + 1
+    if trying1 == 3:
+        break
+while username == "admin":
+    while password != "12345":
+        password = str(input())
+        trying2 = trying2 + 1
+        if trying == 3:
+            break
+        if password == "12345":
+            print("Access granted")
